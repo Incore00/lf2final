eb7c2236580d4bf2f47b26cd15968e22ead46e37
SzymonC <szymon.celej@dfm.com.pl>
2023-11-03 15:57:19 +0100

    asd

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import tkinter as tk
-from toolbar4 import Toolbar, Infobar
+from toolbar4 import Toolbar, Infobar, Leatherpreview, Layerinfo
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-
         self.pack_propagate(0)
         self.grid_propagate(0)
-        self.columnconfigure((1), weight=1)
-        self.rowconfigure(1, weight=25)
-        self.rowconfigure(2, weight=1)
 
         self.toolbar = Toolbar(self)
-        self.toolbar.grid(column=1, row=1, sticky='nsew')
+        self.toolbar.grid(column=1, row=1, columnspan=2, sticky='nsew')
 
         self.infobar = Infobar(self)
-        self.infobar.grid(column=1, row=2, sticky='nsew')
-        #self.infobar.leather_info.configure(text='asd')
+        self.infobar.grid(column=1, row=2, columnspan=2, sticky='nsew')
 
+        self.leather_preview = Leatherpreview(self)
+        self.leather_preview.grid(column=1, row=3, sticky='nsew')
 
+        self.layer_info = Layerinfo(self)
+        self.layer_info.grid(column=2, row=3, sticky='nsew')
 
         self.configure(bg='#303030')
         self.pack_propagate(0)
@@ -30,7 +29,7 @@ if __name__ == '__main__':
     root.iconbitmap("images/icon.ico")
     root.title("LeatherFlaws")
     root.resizable(True, True)
-    #root.state("zoomed")
+    root.state("zoomed")
     MainApplication(root, width=1920, height=1080).pack(side="top", fill="both", expand=True)
     root.mainloop()
 
--- toolbar4.py
+++ toolbar4.py
@@ -2,6 +2,11 @@ import tkinter as tk
 import customtkinter as ctk
 import pyglet
 from tkfontawesome import icon_to_image
+import os
+import pygame
+from PIL import ImageTk
+from datetime import datetime
+from threading import Thread
 
 pyglet.font.add_file('fonts/OpenSans/OpenSans.ttf')
 fontfile = "VeraMono.ttf"
@@ -15,23 +20,54 @@ class Toolbar(tk.Frame):
         self.configure(height=int(parent.winfo_reqheight() * 0.1), width=parent.winfo_reqwidth(), bg='#303030')
         self.pack_propagate(0)
         self.grid_propagate(0)
-        self.columnconfigure((1), weight=1)
+        self.columnconfigure((1, 2, 3, 4, 5, 6, 7), weight=1)
         self.rowconfigure(1, weight=1)
 
-        toolbar_container = tk.Frame(self, height=int(parent.winfo_reqheight() * 0.1), width=parent.winfo_reqwidth(),
-                                     bg='#303030')
-        toolbar_container.pack_propagate(0)
-        toolbar_container.grid_propagate(0)
-        toolbar_container.columnconfigure((1, 2, 3, 4, 5, 6, 7), weight=1)
-        toolbar_container.rowconfigure(1, weight=1)
-        toolbar_container.grid(column=1, row=1, sticky='nsew')
+        logo = ImageTk.PhotoImage(file='images/logo.png')
+        logo_label = tk.Label(self, image=logo, bg='#404040')
+        logo_label.photo = logo
+        logo_label.grid(column=1, row=1, sticky='nsew', ipadx=5)
+
+        self.settings_icon = icon_to_image("cog", fill='#c7c6c5', scale_to_width=60)
+        self.settings_btn = ctk.CTkButton(self, image=self.settings_icon, fg_color='#505050', hover_color='#404040',
+                                          compound='top', corner_radius=10, text='Ustawienia',
+                                          text_font=('OpenSans.ttf', 18))
+        self.settings_btn.grid(column=2, row=1, sticky='nsew')
+
+        self.change_colors_icon = icon_to_image("sync-alt", fill='#c7c6c5', scale_to_width=60)
+        self.change_colors_btn = ctk.CTkButton(self, image=self.change_colors_icon,
+                                               fg_color='#505050',
+                                               hover_color='#404040', compound='top', corner_radius=10,
+                                               text='Zmień kolory', text_font=('OpenSans.ttf', 18))
+        self.change_colors_btn.grid(column=3, row=1, sticky='nsew')
+
+        self.clock = tk.StringVar()
+        tk.Label(self, textvariable=self.clock, bg='#404040', fg='#c7c6c5',
+                 font=('OpenSans.ttf', 19)).grid(column=4, row=1, sticky='nsew', ipadx=5)
+        Thread(target=self.clockLoop()).start()
+
+        self.load_file_icon = icon_to_image("folder-open", fill='#c7c6c5', scale_to_width=60)
+        self.load_file_btn = ctk.CTkButton(self, image=self.load_file_icon, fg_color='#505050',
+                                           hover_color='#404040', compound='top', corner_radius=10, text='Załaduj plik',
+                                           text_font=('OpenSans.ttf', 18))
+        self.load_file_btn.grid(column=5, row=1, sticky='nsew')
+
+    def clockLoop (self):
+        clock = datetime.now().strftime('%Y-%m-%d\n%H:%M:%S') + '\nTydzień ' + str(
+            datetime.isocalendar(datetime.now())[1])
+        self.clock.set(clock)
+        self.after(1000, self.clockLoop)
+
+
+
+
 
 
 class Infobar(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.configure(bg='#303030')
+        self.configure(bg='#303030', width=int(parent.winfo_reqwidth()), height=int(parent.winfo_reqheight() * 0.035))
         self.pack_propagate(0)
         self.grid_propagate(0)
         self.columnconfigure(1, weight=1)
@@ -40,8 +76,48 @@ class Infobar(tk.Frame):
         self.leather_info.insert(tk.END, "LEATHER123123")
         self.leather_info.grid(column=1, row=1, sticky='nsew')
 
-        self.load_file_icon = icon_to_image("folder-open", fill='#c7c6c5', scale_to_width=60)
-        self.load_btn = ctk.CTkButton(self, image=self.load_file_icon, fg_color='#505050',
-                                      hover_color='#404040', command=lambda: self.load_leather()
-                                      , compound='top', corner_radius=10, text='Załaduj plik',
-                                      text_font=('OpenSans.ttf', 18))
+        #self.load_file_icon = icon_to_image("folder-open", fill='#c7c6c5', scale_to_width=60)
+        #self.load_btn = ctk.CTkButton(self, image=self.load_file_icon, fg_color='#505050',
+        #                              hover_color='#404040', command=lambda: self.load_leather()
+        #                              , compound='top', corner_radius=10, text='Załaduj plik',
+        #                              text_font=('OpenSans.ttf', 18))
+
+    def update_info(self, text:str):
+        self.leather_info.insert(tk.END, text)
+
+class Leatherpreview(tk.Frame):
+    def __init__ (self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.pack_propagate(0)
+        self.grid_propagate(0)
+        self.screen_width, self.screen_height = int(parent.winfo_reqwidth()/1.6) , int(parent.winfo_reqheight()/1.6)
+        print(self.screen_width, self.screen_height)
+
+        self.configure(background='brown', width=self.screen_width, height=self.screen_height)
+
+        os.environ['SDL_WINDOWID'] = str(self.winfo_id())
+        os.environ['SDL_VIDEODRIVER'] = 'windib'
+        pygame.display.init()
+        window_size = (int(parent.winfo_reqwidth()/1.618), int(parent.winfo_reqheight()/1.618))
+        self.screen = pygame.display.set_mode(window_size)
+        self.main_surface = pygame.Surface(window_size)
+
+
+
+        self.pygame_loop()
+
+    def pygame_loop (self):
+        pygame.display.flip()
+
+        self.update()
+        self.after(1, self.pygame_loop)
+
+class Layerinfo(tk.Frame):
+    def __init__ (self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.pack_propagate(0)
+        self.grid_propagate(0)
+
+        self.configure(background='green', width=int(parent.winfo_reqwidth()-(parent.winfo_reqwidth()/1.618)), height=int(parent.winfo_reqheight()*0.865))
